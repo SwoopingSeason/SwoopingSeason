ae1f39a456a9dd2e84c89b66458d9b86bc57247a
Daniel Manganaro <manganaro.daniel@gmail.com>
2018-07-18 13:16:37 +0800

    Initial Project Setup

---
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject boomerangPrefab;
+
+    private bool holdingBoomerang; 
+
+
+	// Use this for initialization
+	void Start () {
+        holdingBoomerang = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            if (holdingBoomerang == true) {
+                Debug.Log("Throw Boomerang");
+            }            
+        }
+	}
+}
---
+++ Assets/_Game/Scripts/Systems/CameraResize.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        ResizeCamera();
+	}
+
+    private void Update()
+    {
+        ResizeCamera();
+    }
+
+    void ResizeCamera()
+    {
+        // set the desired aspect ratio
+        float targetAspect = 16.0f / 9.0f;
+
+        // determine the game window's current aspect ratio
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleHeight = windowAspect / targetAspect;
+
+        // obtain camera component so we can modify its viewport
+        Camera myCamera = GetComponent<Camera>();
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleHeight < 1.0f) {
+            Rect rect = myCamera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+
+            myCamera.rect = rect;
+        } else // add pillarbox
+          {
+            float scaleWidth = 1.0f / scaleHeight;
+
+            Rect rect = myCamera.rect;
+
+            rect.width = scaleWidth;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+
+            myCamera.rect = rect;
+        }
+    }
+}
