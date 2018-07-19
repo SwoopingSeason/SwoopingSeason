586a9068f41da0e8f52dac5b95644ad6aa2abaa9
Daniel Manganaro <manganaro.daniel@gmail.com>
2018-07-19 19:01:53 +0800

    Boomerang Setup

--- Assets/_Game/Scripts/Player/BoomerangScript.cs
+++ Assets/_Game/Scripts/Player/BoomerangScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoomerangScript : MonoBehaviour {
+    public float rotateSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,7 @@ public class BoomerangScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Rotate the object around its local z axis
+        transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f) * Time.deltaTime * rotateSpeed);
 	}
 }
--- Assets/_Game/Scripts/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public GameObject boomerangPrefab;
+    public Transform boomerang;
 
     private bool holdingBoomerang; 
-
-
+    
 	// Use this for initialization
 	void Start () {
         holdingBoomerang = true;
@@ -16,9 +15,25 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButton(0)) {
+
+            Vector3 mousePos = Input.mousePosition;
+
             if (holdingBoomerang == true) {
-                Debug.Log("Throw Boomerang");
+                //Debug.Log("Throw Boomerang");
+                Debug.Log(mousePos);
+
+                Vector3 startPos = mousePos;
+
+                // Start Pos in between x -0.9 to 0.9
+                // and y between -3.2 to -2.6
+
+                ThrowBoomerang(startPos);
             }            
         }
 	}
+
+    void ThrowBoomerang(Vector3 startPos)
+    {
+        Instantiate(boomerang, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity);
+    }
 }
